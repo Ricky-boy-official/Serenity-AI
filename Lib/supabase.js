889a3ce1854f60c5177382c7bfc86f8a725c97f7
Ricky-boy-official <rickyboyofficial@gmail.com>
2025-05-22 02:10:44 +0100

    Update supabase client with actual URL and anon key

--- Lib/supabase.js
+++ Lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://dacnrqyqerpsbwavmuiq.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhY25ycXlxZXJwc2J3YXZtdWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NzA1MDUsImV4cCI6MjA2MzE0NjUwNX0.78N2z6tKR6oIBfepxmZjxGXW032SsxBnDODgyhyaJ2k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
